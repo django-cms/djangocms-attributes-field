fe68d29ab78db5885bc31b67cf0537f1f02b33ad
Fabian Braun <fsbraun@gmx.de>
2024-11-19 08:54:47 +0100

    feat: Add form field validation from model field (#56)
    * Add pypi actions
    
    * bump version
    
    * feat: Move field validation to form field
    
    * Bump version
    
    * Fix syntax error in test action
    
    * For action fixes
    
    * Update pypi actions

--- djangocms_attributes_field/fields.py
+++ djangocms_attributes_field/fields.py
@@ -14,12 +14,16 @@ from .widgets import AttributesWidget
 regex_key_validator = RegexValidator(regex=r'^[a-z][-a-z0-9_:]*\Z',
                                      flags=re.IGNORECASE, code='invalid')
 
+default_excluded_keys = [
+    "src", "href", "data", "action", "on*",
+]
 
 class AttributesFormField(forms.CharField):
     empty_values = [None, '']
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('widget', AttributesWidget)
+        self.excluded_keys = kwargs.pop('excluded_keys', []) + default_excluded_keys
         super().__init__(*args, **kwargs)
 
     def to_python(self, value):
@@ -37,6 +41,49 @@ class AttributesFormField(forms.CharField):
         # This is required in older django versions.
         if value in self.empty_values and self.required:
             raise forms.ValidationError(self.error_messages['required'], code='required')
+        if isinstance(value, dict):
+            for key, val in value.items():
+                self.validate_key(key)
+                self.validate_value(key, val)
+
+    def validate_key(self, key):
+        """
+        A key must start with a letter, but can otherwise contain letters,
+        numbers, dashes, colons or underscores. It must not also be part of
+        `excluded_keys` as configured in the field.
+
+        :param key: (str) The key to validate
+        """
+        # Verify the key is not one of `excluded_keys`.
+        for excluded_key in self.excluded_keys:
+            if key.lower() == excluded_key or excluded_key.endswith("*") and key.lower().startswith(excluded_key[:-1]):
+                raise ValidationError(
+                    _('"{key}" is excluded by configuration and cannot be used as '
+                      'a key.').format(key=key))
+        # Also check that it fits our permitted syntax
+        try:
+            regex_key_validator(key)
+        except ValidationError:
+            # Seems silly to catch one then raise another ValidationError, but
+            # the RegExValidator doesn't use placeholders in its error message.
+            raise ValidationError(
+                _('"{key}" is not a valid key. Keys must start with at least '
+                  'one letter and consist only of the letters, numbers, '
+                  'underscores or hyphens.').format(key=key))
+
+    def validate_value(self, key, value):
+        """
+        A value can be anything that can be JSON-ified.
+
+        :param key: (str) The key of the value
+        :param value: (str) The value to validate
+        """
+        try:
+            json.dumps(value)
+        except (TypeError, ValueError):
+            raise ValidationError(
+                _('The value for the key "{key}" is invalid. Please enter a '
+                  'value that can be represented in JSON.').format(key=key))
 
 
 class AttributesField(models.Field):
@@ -64,7 +111,7 @@ class AttributesField(models.Field):
             kwargs['default'] = kwargs.get('default', dict)
         excluded_keys = kwargs.pop('excluded_keys', [])
         # Note we accept uppercase letters in the param, but the comparison
-        # is not case sensitive. So, we coerce the input to lowercase here.
+        # is not case-sensitive. So, we coerce the input to lowercase here.
         self.excluded_keys = [key.lower() for key in excluded_keys]
         super().__init__(*args, **kwargs)
         self.validate(self.get_default(), None)
@@ -75,6 +122,7 @@ class AttributesField(models.Field):
             'widget': AttributesWidget
         }
         defaults.update(**kwargs)
+        defaults["excluded_keys"] = self.excluded_keys
         return super().formfield(**defaults)
 
     def from_db_value(self, value,
@@ -133,48 +181,6 @@ class AttributesField(models.Field):
             self.get_prep_value(value)
         except ValueError:
             raise ValidationError(self.error_messages['invalid'] % value)
-
-        for key, val in value.items():
-            self.validate_key(key)
-            self.validate_value(key, val)
-
-    def validate_key(self, key):
-        """
-        A key must start with a letter, but can otherwise contain letters,
-        numbers, dashes, colons or underscores. It must not also be part of
-        `excluded_keys` as configured in the field.
-
-        :param key: (str) The key to validate
-        """
-        # Verify the key is not one of `excluded_keys`.
-        if key.lower() in self.excluded_keys:
-            raise ValidationError(
-                _('"{key}" is excluded by configuration and cannot be used as '
-                  'a key.').format(key=key))
-        # Also check that it fits our permitted syntax
-        try:
-            regex_key_validator(key)
-        except ValidationError:
-            # Seems silly to catch one then raise another ValidationError, but
-            # the RegExValidator doesn't use placeholders in its error message.
-            raise ValidationError(
-                _('"{key}" is not a valid key. Keys must start with at least '
-                  'one letter and consist only of the letters, numbers, '
-                  'underscores or hyphens.').format(key=key))
-
-    def validate_value(self, key, value):
-        """
-        A value can be anything that can be JSON-ified.
-
-        :param key: (str) The key of the value
-        :param value: (str) The value to validate
-        """
-        try:
-            json.dumps(value)
-        except (TypeError, ValueError):
-            raise ValidationError(
-                _('The value for the key "{key}" is invalid. Please enter a '
-                  'value that can be represented in JSON.').format(key=key))
 
     def value_to_string(self, obj):
         return self.value_from_object(obj)
